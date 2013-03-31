abac5342e8356eb777d0c31a1a49edf204730009
shieldsjared <shieldsjared@gmail.com>
2013-03-31 17:27:40 -0400

    Established a facade (MathUtil) to expose our underlying mathematical functions. Established a class to store the expression currently stored by the calculator.

---
+++ Calculator/src/main/java/edu/odu/cs/cs600/calculator/math/ExpressionEvaluator.java
@@ -0,0 +1,10 @@
+package edu.odu.cs.cs600.calculator.math;
+
+public class ExpressionEvaluator {
+	
+	public static double evaluate(String expression)
+	{
+		return 0;
+	}
+
+}
--- Calculator/src/main/java/edu/odu/cs/cs600/calculator/operations/ISingleValueOperation.java
+++
@@ -1,7 +0,0 @@
-package edu.odu.cs.cs600.calculator.operations;
-
-public interface ISingleValueOperation extends IOperation {
-	
-	public double Execute(double x, double y);
-
-}
