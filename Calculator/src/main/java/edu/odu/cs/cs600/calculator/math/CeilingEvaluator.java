2d04450760a637d66e25732238bcb2e937e7073d
shieldsjared <shieldsjared@gmail.com>
2013-03-31 20:35:11 -0400

    Adding stubbed out classes to manage the various functions for the calculator.

---
+++ Calculator/src/main/java/edu/odu/cs/cs600/calculator/math/CeilingEvaluator.java
@@ -0,0 +1,10 @@
+package edu.odu.cs.cs600.calculator.math;
+
+public class CeilingEvaluator {
+
+	public static double compute(double value)
+	{
+		return 0;
+	}
+	
+}
---
+++ Calculator/src/main/java/edu/odu/cs/cs600/calculator/math/FloorEvaluator.java
@@ -0,0 +1,10 @@
+package edu.odu.cs.cs600.calculator.math;
+
+public class FloorEvaluator {
+
+	public static double compute(double value)
+	{
+		return 0;
+	}
+	
+}
