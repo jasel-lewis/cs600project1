29c3c0eb5fa821834306db32f4d906f5a0e85c5e
jasel <jasel@jasel.com>
2013-03-31 20:58:07 -0400

    Incremental push

--- Calculator/src/main/java/edu/odu/cs/cs600/calculator/CalculatorController.java
+++ Calculator/src/main/java/edu/odu/cs/cs600/calculator/CalculatorController.java
@@ -1,5 +1,37 @@
 package edu.odu.cs.cs600.calculator;
 
+import java.awt.Color;
+import java.util.ArrayList;
+
+import edu.odu.cs.cs600.calculator.gui.CalculatorCharacter;
+import edu.odu.cs.cs600.calculator.gui.CalculatorView;
+
 public class CalculatorController {
-	;
+	public CalculatorController(CalculatorModel model, CalculatorView view) {
+		;
+	}
+	
+	
+	
+	/**
+	 * Place this CalculatorDisplay into the "on" isOn for the calculator.  Functionality
+	 * is the same as what {@link #clear() clear()} performs.
+	 */
+	public void onState() {
+		setForeground(Color.DARK_GRAY);
+		clear();
+	}  // end onState()
+	
+	
+	
+	/**
+	 * Place this CalculatorDisplay into the "off" isOn for the calculator.  The
+	 * phrase is emptied of all characters and the display is updated with such
+	 * (presenting the user with a blank display).
+	 */
+	public void offState() {
+		dcList = (ArrayList<CalculatorCharacter>) OFF.clone();
+		setForeground(Color.LIGHT_GRAY);
+		update();
+	}  // end offState()
 }
