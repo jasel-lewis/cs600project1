bddca1119c269f4456269a442d8cbe3e86dd4aa9
jasel <jasel@jasel.com>
2013-04-02 14:11:22 -0400

    Incremental commit before I head out

--- Calculator/src/main/java/edu/odu/cs/cs600/calculator/gui/OperatorButton.java
+++ Calculator/src/main/java/edu/odu/cs/cs600/calculator/gui/OperatorButton.java
@@ -1,25 +1,10 @@
 package edu.odu.cs.cs600.calculator.gui;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.AbstractAction;
-import javax.swing.ActionMap;
-import javax.swing.ComponentInputMap;
-import javax.swing.InputMap;
-import javax.swing.JComponent;
-import javax.swing.KeyStroke;
-import javax.swing.SwingUtilities;
-import javax.swing.plaf.ActionMapUIResource;
-
-import edu.odu.cs.cs600.calculator.Application;
-
 public class OperatorButton extends CalculatorButton {
 	
 	private static final long serialVersionUID = 7293030709752505015L;
 	
 	private CalculatorCharacter cc;
-	private CalculatorDisplay display;
 	
 	
 	public OperatorButton(String imageFilenamePath, String fallbackText, char morpheme) {
@@ -33,13 +18,4 @@ public class OperatorButton extends CalculatorButton {
 	public CalculatorCharacter getDisplayCharacter() {
 		return cc;
 	}  // end getCalculatorCharacter()
-	
-
-	/**
-	 * Return the {@link CalculatorDisplay} assigned to this button
-	 * @return
-	 */
-	public CalculatorDisplay getDisplay() {
-		return display;
-	}  // end getDisplay()
 }  // end class CharacterButton
