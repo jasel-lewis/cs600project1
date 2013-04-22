fced674e3c4e806d637336398c7326107db68e9e
Jasel <jasel@jasel.com>
2013-04-22 12:47:09 -0400

    - Found error: AdditionEvaluator was copied over from MultiplicationEvaluator and I forgot to change '*' to '+'.  Me = Dumbass - Added in Log4J with debug statements to OperatorExpression

--- Calculator/src/main/java/edu/odu/cs/cs600/calculator/math/AdditionEvaluator.java
+++ Calculator/src/main/java/edu/odu/cs/cs600/calculator/math/AdditionEvaluator.java
@@ -3,6 +3,6 @@ package edu.odu.cs.cs600.calculator.math;
 public class AdditionEvaluator implements IBinaryEvaluator {
 
 	public double compute(double x, double y) throws ArithmeticException {
-		return x * y;
+		return x + y;
 	}
 }
--- Calculator/src/main/java/edu/odu/cs/cs600/calculator/math/grammar/expressions/OperatorExpression.java
+++ Calculator/src/main/java/edu/odu/cs/cs600/calculator/math/grammar/expressions/OperatorExpression.java
@@ -1,5 +1,8 @@
 package edu.odu.cs.cs600.calculator.math.grammar.expressions;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
 import edu.odu.cs.cs600.calculator.math.MathUtil;
 import edu.odu.cs.cs600.calculator.math.grammar.TokenType;
 import edu.odu.cs.cs600.calculator.math.grammar.exceptions.ParseException;
@@ -8,6 +11,7 @@ import edu.odu.cs.cs600.calculator.math.grammar.exceptions.ParseException;
  * Taken from: https://github.com/munificent/bantam/blob/master/src/com/stuffwithstuff/bantam/expressions/OperatorExpression.java
  */
 public class OperatorExpression implements Expression {
+	private static Logger logger = LogManager.getLogger(OperatorExpression.class);
 	private final Expression left;
 	private final Expression right;
 	private final TokenType operator;
@@ -33,19 +37,33 @@ public class OperatorExpression implements Expression {
 	
 	@Override
 	public double getValue() {
+		double leftValue = left.getValue();
+		double rightValue = right.getValue();
+		double result;
+		
 		switch (operator) {
 			case PLUS:
-				return (MathUtil.add(left.getValue(), right.getValue()));
+				result = MathUtil.add(leftValue, rightValue); 
+				logger.debug("Parser evaluating " + leftValue + " + " + rightValue + " and getting: " + result);
+				return result;
 			case MINUS:
-				return (MathUtil.subtract(left.getValue(), right.getValue()));
+				result = MathUtil.subtract(leftValue, rightValue); 
+				logger.debug("Parser evaluating " + leftValue + " - " + rightValue + " and getting: " + result);
+				return result;
 			case MULTIPLY:
-				return (MathUtil.multiply(left.getValue(), right.getValue()));
+				result = MathUtil.multiply(leftValue, rightValue); 
+				logger.debug("Parser evaluating " + leftValue + " * " + rightValue + " and getting: " + result);
+				return result;
 			case DIVIDE:
-				return (MathUtil.divide(left.getValue(), right.getValue()));
+				result = MathUtil.divide(leftValue, rightValue); 
+				logger.debug("Parser evaluating " + leftValue + " / " + rightValue + " and getting: " + result);
+				return result;
 			case POWER:
 				try {
-					int exponent = Integer.parseInt(Double.toString(right.getValue()));
-					return (MathUtil.exponentiate(left.getValue(), exponent));
+					int exponent = Integer.parseInt(Double.toString(rightValue));
+					result = MathUtil.exponentiate(leftValue, exponent); 
+					logger.debug("Parser evaluating " + leftValue + " ^ " + exponent + " and getting: " + result);
+					return result;
 				} catch (NumberFormatException nfe) {
 					throw new ParseException("Exponent must be an integer");
 				}
