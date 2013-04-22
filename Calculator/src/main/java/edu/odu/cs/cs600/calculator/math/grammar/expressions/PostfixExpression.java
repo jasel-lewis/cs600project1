02c399066bb9ec2a44c18f3b3af7f6a3db5fac71
Jasel <jasel@jasel.com>
2013-04-22 10:48:30 -0400

    Just comment update for a TODO

--- Calculator/src/main/java/edu/odu/cs/cs600/calculator/math/grammar/expressions/PostfixExpression.java
+++ Calculator/src/main/java/edu/odu/cs/cs600/calculator/math/grammar/expressions/PostfixExpression.java
@@ -9,6 +9,9 @@ import edu.odu.cs.cs600.calculator.math.grammar.exceptions.ParseException;
  * 
  * Taken from: https://github.com/munificent/bantam/blob/master/src/com/stuffwithstuff/bantam/expressions/PrefixExpression.java
  */
+// TODO: This entire postfix implementation may be removed if we succinctly decide
+// that factorial is only executed on button-press (Phrase evaluated then factorial
+// applied) as opposed to being evaluated as somewhere internal to the Phrase.
 public class PostfixExpression implements Expression {
 	private final TokenType operator;
 	private final Expression left;
