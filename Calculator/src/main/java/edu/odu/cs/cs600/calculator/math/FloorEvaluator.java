79847a2be1310ddac91585466df9053d397ad71c
jasel <jasel@jasel.com>
2013-04-27 08:59:35 -0400

    I'm pretty solid on the floor function - just a few more, randomized tests to be certain.

--- Calculator/src/main/java/edu/odu/cs/cs600/calculator/math/FloorEvaluator.java
+++ Calculator/src/main/java/edu/odu/cs/cs600/calculator/math/FloorEvaluator.java
@@ -7,9 +7,102 @@ public class FloorEvaluator implements IUnaryEvaluator {
 
 	private static Logger logger = LogManager.getLogger(FloorEvaluator.class);
 	
-	public double compute(double value) throws ArithmeticException
-	{
-		logger.error("Floor function has not yet been completed and is using a JAVA OOTB function right now.");
-		return Math.floor(value);
-	}	
+	/**
+	 * Determine the floor of the passed value.  Divide the realm of infinite, real numbers
+	 * into a range; bracketing value by two binary values where 2^k <= value < 2^(k+1).
+	 * Perform a binary search for value within this range.  Method handles special cases
+	 * such as 0.0 >= value < 1.0 and -1.0 >= value < 0.0 as these can't be encapsulated by
+	 * the above process.  Negative input is passed to {@link MathUtil.ceiling} under the
+	 * realization that floor(a) = -(ceiling(-a)). 
+	 * @param value
+	 * @return
+	 * @throws ArithmeticException
+	 */
+	public double compute(double value) throws ArithmeticException {
+		logger.debug("Computing the floor on value: " + value);
+		
+		if (value >= 1.0) {
+			int k = 0, kPlusOne = 1;
+			
+			while (binaryExponentiation(kPlusOne) <= value) {
+				k = kPlusOne++;
+			}
+			
+			logger.debug("Bounded value with 2^" + k + " and 2^" + kPlusOne);
+			
+			return (double)floorBinarySearch(binaryExponentiation(k), binaryExponentiation(kPlusOne), value);
+		} else if ((value >= 0.0) && (value < 1.0)) {
+			logger.debug("Special case handled: vaue is >= 0.0 && < 1.0");
+			return (0.0);
+		} else if ((value >= MathUtil.negate(1.0)) && (value < 0.0)) {
+			logger.debug("Special case handled: value is >= -1.0 && < 0.0");
+			return (MathUtil.negate(1.0));
+		} else {
+			logger.debug("Value is negative; sending to ceiling() with a negated value");
+			
+			// If a < 0, Floor(a) = -(Ceiling(-a))
+			return (MathUtil.negate(MathUtil.ceiling(MathUtil.negate(value))));
+		}
+	}
+	
+	
+	
+	/**
+	 * Perform a binary search for value within the linear set of numbers delineated by lower
+	 * and upper; returning the lower bound
+	 * @param lower lower bound of the integer range to which value is greater than, <i>or equal to</i>
+	 * @param upper upper bound of the integer range to which value is less than
+	 * @param value
+	 * @return
+	 */
+	private int floorBinarySearch(int lower, int upper, double value) {
+		logger.debug("Performing a binary search on the range " + lower + " to " + upper + " looking for " + value);
+		
+		if (upper != (lower + 1)) {
+			// Find the middle value
+			int middle = (int)((lower + upper) * 0.5);
+			logger.debug("Middle of the range was determined to be " + middle);
+			
+			// Does value exist in the left or right partition of the range?
+			if (value < middle) {
+				logger.debug("Value in lower half of range - making recursive call on lower range (" + lower + " to " + middle + ")");
+				return(floorBinarySearch(lower, middle, value));
+			} else {
+				logger.debug("Value in upper half of range - making recursive call on upper range (" + middle + " to " + upper + ")");
+				return(floorBinarySearch(middle, upper, value));
+			}
+		}
+		
+		return lower;
+	}
+	
+	
+	
+	/**
+	 * This method accepts a positive integer value as the exponent with which to
+	 * perform binary exponentiation.  MathUtil.exponentiate() could not be utilized
+	 * as this would have set up a circular dependency ({@link ExponentialEvaluator}
+	 * relies on MathUtil.floor() which then relies on this class).  If a negative
+	 * value is passed, an {@link ArithmeticException} is thrown.
+	 * @param exponent
+	 * @return
+	 * @throws {@link ArithmeticException}
+	 */
+	private int binaryExponentiation(int exponent) {
+		if (exponent == 0) {
+			return 1;
+		} else if (exponent == 1) {
+			return 2;
+		} else if (exponent > 1) {
+			int result = 2;
+			
+			while (exponent-- > 1) {
+				result = result * 2;
+			}
+			
+			return result;
+		} else {
+			throw new ArithmeticException("Argument must be positive");
+		}
+	}
 }
---
+++ Calculator/src/test/java/edu/odu/cs/cs600/calculator/math/FloorEvaluationTest.java
@@ -0,0 +1,56 @@
+package edu.odu.cs.cs600.calculator.math;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class FloorEvaluationTest {
+	// For double comparison, this is the precision to which assertEquals will compare values
+	private static final double EPSILON = 1e-10;
+	
+	private double value;
+	
+	
+	public FloorEvaluationTest(double value) {
+		this.value = value;
+	}
+	
+	
+	@Parameters
+	public static Collection<Object[]> testData() {
+		// { expression, expectedResult }
+		return Arrays.asList(new Object[][] {
+			{ 1.0 },			//  0
+			{ 1.2 },			//  1
+			{ 1.23456789 },		//  2
+			{ 9.9 },			//  3
+			{ 24567.123 },		//  4
+			{ 0.0 },			//  5
+			{ 0.1 },			//  6
+			{ 0.100000 },		//  7
+			{ 0.9 },			//  8
+			{ 0.999999 },		//  9
+			{ -1.3 },			// 10
+			{ -1.9 },			// 11
+			{ -0.1 },			// 12
+			{ -0.0000001 },		// 13
+			{ -1.00000000 },	// 14
+			{ -9.99999999 }		// 15
+		});
+	}
+	
+	
+	@Test
+	public void testValidNumbers() {
+		assertEquals(Math.floor(value), MathUtil.floor(value), EPSILON);
+	}
+}
