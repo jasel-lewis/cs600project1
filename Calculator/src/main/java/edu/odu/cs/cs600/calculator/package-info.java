3af77ba8272e7c7861d20892b98ccf113fa8595f
shieldsjared <shieldsjared@gmail.com>
2013-04-28 15:14:34 -0400

    Removed a lingering TODO. Began stubbing out the README.

--- Calculator/src/main/java/edu/odu/cs/cs600/calculator/package-info.java
+++ Calculator/src/main/java/edu/odu/cs/cs600/calculator/package-info.java
@@ -1,4 +1,3 @@
-//TODO: JS - Create the rest of the package-info files to explain the purpose of each package.
 /**
  * Packages contains all classes/sub-packages related to the Calculator project
  * assigned during the Spring 2013 semester, in CS600 (as instructed by Dr. Olariu).
