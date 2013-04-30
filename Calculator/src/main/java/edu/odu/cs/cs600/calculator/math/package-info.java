f287257749af92e0e3d4c003bb7360f8791f9828
jasel <jasel@jasel.com>
2013-04-29 21:24:31 -0400

    Spelling correction

--- Calculator/src/main/java/edu/odu/cs/cs600/calculator/math/package-info.java
+++ Calculator/src/main/java/edu/odu/cs/cs600/calculator/math/package-info.java
@@ -1,6 +1,6 @@
 /**
  * The math package is the root of all math related functions.  Within this direct package 
- * are various "Evaluators" which explicitly breakup the implimentation of various
+ * are various "Evaluators" which explicitly breakup the implementation of various
  * mathematical functions.
  */
 package edu.odu.cs.cs600.calculator.math;
